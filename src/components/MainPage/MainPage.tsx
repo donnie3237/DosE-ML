bce72bad15de3170321ef0e53fc95c359877aabd
Sora <paradorn3237@gmail.com>
2023-01-01 22:18:27 +0700

    .

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import './mainPage.css'
+import './mainPage.scss'
 
 function MainPage() {
   return (
     <div className='mp'>
-        <h1></h1>
-
+        <h1>Hello Welcome to Machine Learning World!!</h1>
+        <h2>ที่นี่เราจะอธิบายหลักการของ Machinelearning</h2>
+        <h2>และเป็นแหล่งปล่อยโมเดลของฉัน</h2>
+        <h3>หมายเหตุ : อาจPredict ช้าหน่อยในบางโมเดล เพราะ ระบบประมวลผลของเซิฟเวอร์มันไม่ค่อยดีซักเท่าไหร่นัก</h3>
+        <h4>Auth : DosE</h4>
     </div>
   )
 }
